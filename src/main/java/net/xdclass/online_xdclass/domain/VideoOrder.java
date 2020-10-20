b950c7e9e0b6220e2637d80c13b6385744361cb6
shenhengxin <shenhengxin@wshifu.com>
2020-10-20 17:09:16 +0800

    项目相关包和实体类创建

--- src/main/java/net/xdclass/online_xdclass/domain/VideoOrder.java
+++ src/main/java/net/xdclass/online_xdclass/domain/VideoOrder.java
@@ -2,18 +2,13 @@ package net.xdclass.online_xdclass.domain;
 
 import java.util.Date;
 
+
 /**
- * 小滴课堂 订单对象
- * `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
- *   `out_trade_no` varchar(64) DEFAULT NULL COMMENT '订单唯一标识',
- *   `state` int(11) DEFAULT NULL COMMENT '0表示未支付，1表示已支付',
- *   `create_time` datetime DEFAULT NULL COMMENT '订单生成时间',
- *   `total_fee` int(11) DEFAULT NULL COMMENT '支付金额，单位分',
- *   `video_id` int(11) DEFAULT NULL COMMENT '视频主键',
- *   `video_title` varchar(256) DEFAULT NULL COMMENT '视频标题',
- *   `video_img` varchar(256) DEFAULT NULL COMMENT '视频图片',
- *   `user_id` int(12) DEFAULT NULL COMMENT '用户id',
- */
+* @ClassName: VideoOrder
+* @Description: TODO
+* @author: iwanli
+* @date: 2020/10/20 8:34
+*/
 public class VideoOrder {
 
     private Integer id;
@@ -51,6 +46,12 @@ public class VideoOrder {
                 '}';
     }
 
+    /**
+    * @Description: TODO
+    * @author: iwanli
+    * @date: 2020/10/19 17:49
+    * @Return: java.lang.Integer
+    */
     public Integer getId() {
         return id;
     }
