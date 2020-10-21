af137dd362b553dc207ecdf0cba28265f49764fc
shenhengxin <853043009@qq.com>
2020-10-21 13:44:27 +0800

    视频列表接口开发+api权限路径规划

--- src/main/java/net/xdclass/online_xdclass/controller/VideoController.java
+++ src/main/java/net/xdclass/online_xdclass/controller/VideoController.java
@@ -2,6 +2,7 @@ package net.xdclass.online_xdclass.controller;
 
 import net.xdclass.online_xdclass.domain.Video;
 import net.xdclass.online_xdclass.service.VideoService;
+import net.xdclass.online_xdclass.utils.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +17,14 @@ import java.util.List;
  * @Date: 2020-10-21 13:12  //时间
  */
 @RestController
-@RequestMapping("video")
+@RequestMapping("api/v1/pub/video")
 public class VideoController {
     @Autowired
     private VideoService videoService;
 
     @GetMapping("list")
-    public List<Video> listVideo(){
+    public JsonData listVideo(){
         List<Video> listVideo = videoService.listVideo();
-        return listVideo;
+        return JsonData.buildSuccess(listVideo);
     }
 }
